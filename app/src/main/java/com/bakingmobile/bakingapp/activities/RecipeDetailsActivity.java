44e2db731e71723d8c9869df4a7372a9e55e4261
blessochampion <Blessing.Ekundayo@stash.interswitch.com>
2017-07-02 13:37:19 +0100

    add recipe_details_activiy implementation

--- app/src/main/java/com/bakingmobile/bakingapp/activities/RecipeDetailsActivity.java
+++ app/src/main/java/com/bakingmobile/bakingapp/activities/RecipeDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.bakingmobile.bakingapp.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,46 +8,64 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.bakingmobile.bakingapp.R;
+import com.bakingmobile.bakingapp.adapters.RecipeListDetailsAdapter;
 import com.bakingmobile.bakingapp.models.Ingredient;
 import com.bakingmobile.bakingapp.models.Recipe;
+import com.bakingmobile.bakingapp.models.Step;
 
 import java.util.ArrayList;
 
-public class RecipeDetailsActivity extends AppCompatActivity {
+public class RecipeDetailsActivity extends AppCompatActivity implements RecipeListDetailsAdapter.RecipeStepItemTouchListener {
 
     public static final String KEY_RECIPE = "recipe";
     Recipe mRecipe;
-    private TextView mIngredients;
+
+    private RecyclerView mRecipeListSteps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
 
+        mRecipeListSteps = (RecyclerView) findViewById(R.id.rv_recipe_list_steps);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mRecipeListSteps.setLayoutManager(linearLayoutManager);
+
+
         //get references to views
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        mIngredients = (TextView) findViewById(R.id.tv_ingredients);
+        mRecipeListSteps = (RecyclerView) findViewById(R.id.rv_recipe_list_steps);
 
         Intent intentThatStartedThisActivity = getIntent();
         if(intentThatStartedThisActivity.hasExtra(KEY_RECIPE)){
-
             mRecipe = intentThatStartedThisActivity.getParcelableExtra(KEY_RECIPE);
+
             getSupportActionBar().setTitle(mRecipe.getName());
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeButtonEnabled(true);
-            showIngredients();
+            showRecipeDetails();
 
         }else{
             finish();
         }
 
 
+
+
+    }
+
+    private void showRecipeDetails() {
+        ArrayList<Ingredient> ingredients = mRecipe.getIngredients();
+        ArrayList<Step> steps =  mRecipe.getSteps();
+        Context appContext = getApplicationContext();
+
+        RecipeListDetailsAdapter recipeStepsAdapter = new RecipeListDetailsAdapter(appContext, ingredients, steps);
+        recipeStepsAdapter.setRecipeStepOnTouchListener(this);
+        mRecipeListSteps.setAdapter(recipeStepsAdapter);
 
 
     }
@@ -60,25 +79,9 @@ public class RecipeDetailsActivity extends AppCompatActivity {
         return  super.onOptionsItemSelected(item);
     }
 
-    private void showIngredients() {
 
-        ArrayList<Ingredient> ingredients = mRecipe.getIngredients();
-        String formattedIngredient;
-        int quantity;
-        String measure;
-        String ingredientDetails;
-
-        for (Ingredient ingredient : ingredients){
-            formattedIngredient  = getString(R.string.bullet);
-            quantity = ingredient.getQuantity();
-            measure = ingredient.getMeasure();
-            ingredientDetails = ingredient.getIngredient();
-            formattedIngredient += " " + ingredientDetails + " ("+ quantity +" " + measure + ")";
-            mIngredients.append(formattedIngredient+ "\n\n");
-
-        }
-
-
+    @Override
+    public void onRecipeStepItemTouched(Step touchedRecipeStep) {
+        /*todo start another activity*/
     }
-
 }
---
+++ app/src/main/java/com/bakingmobile/bakingapp/adapters/RecipeListDetailsAdapter.java
@@ -0,0 +1,171 @@
+package com.bakingmobile.bakingapp.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bakingmobile.bakingapp.R;
+import com.bakingmobile.bakingapp.models.Ingredient;
+import com.bakingmobile.bakingapp.models.Step;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by blessochampion on 6/29/17.
+ */
+
+public class RecipeListDetailsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<Step> steps;
+    private List<Ingredient> ingredients;
+    private Context applicationContext;
+    private RecipeStepItemTouchListener listener;
+
+    private static final int INGREDIENT_POSITION = 0;
+
+    public static interface RecipeStepItemTouchListener {
+        void onRecipeStepItemTouched(Step touchedRecipeStep);
+    }
+
+
+    public RecipeListDetailsAdapter(Context context, List<Ingredient> ingredients, List<Step> steps) {
+        this.steps = steps;
+        applicationContext = context;
+        this.ingredients = ingredients;
+
+    }
+
+    public void setRecipeStepOnTouchListener(RecipeStepItemTouchListener recipeStepOnTouchListener) {
+        listener = recipeStepOnTouchListener;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if(viewType == INGREDIENT_POSITION){
+            view  = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_ingredient_list, parent, false);
+            return new IngredientViewHolder(view, applicationContext);
+        }else {
+             view  = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_step_list_item, parent, false);
+            return new StepViewHolder(applicationContext, view, listener);
+        }
+
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return  position;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if(position == INGREDIENT_POSITION){
+            ((IngredientViewHolder) holder).bindView(ingredients);
+        }else {
+            int stepPosition = position-1;
+            Step currentStep = steps.get(stepPosition);
+            boolean isLastPosition = (stepPosition == steps.size()-1);
+            Log.e("dd", isLastPosition+"");
+            ((StepViewHolder)holder).bindView(currentStep, stepPosition, isLastPosition);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return steps.size()+1;
+    }
+
+    public static class StepViewHolder extends RecyclerView.ViewHolder {
+        TextView mShortDescription;
+        Context context;
+        RecipeStepItemTouchListener mTouchListener;
+        View mItemView;
+        View mDivider;
+
+        public StepViewHolder(Context context, View itemView, RecipeStepItemTouchListener listener) {
+            super(itemView);
+            this.context = context;
+            mTouchListener = listener;
+            mItemView = itemView;
+            mShortDescription = (TextView) itemView.findViewById(R.id.tv_recipe_step_short_description);
+            mDivider = itemView.findViewById(R.id.v_divider);
+
+
+        }
+
+        public void bindView(final Step step, int position) {
+            String stepDescription = position +".  " + step.getShortDescription();
+            mShortDescription.setText(stepDescription);
+
+            /*todo set image/video icon or description that shows there is no message*/
+
+           /* if (recipe.hasImage()) {
+                ImageUtils.loadImageFromRemoteServerIntoImageView(
+                        context, recipe.getImageURL(), mImageView
+                );
+
+            } else {
+                ImageUtils.loadImageFromResourcesToImageView(
+                        context, R.drawable.recipe_image, mImageView
+                );
+            }*/
+
+            mItemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mTouchListener != null)
+                        mTouchListener.onRecipeStepItemTouched(step);
+                }
+            });
+
+        }
+
+        public void bindView(final Step step, int position, boolean isLastItem){
+            bindView(step, position);
+
+            if(isLastItem){
+                mDivider.setVisibility(View.GONE);
+            }
+        }
+
+
+    }
+
+    public static class IngredientViewHolder extends RecyclerView.ViewHolder{
+        private TextView mIngredients;
+        private Context mApplicationContext;
+
+        public IngredientViewHolder(View itemView, Context applicationContext) {
+            super(itemView);
+            mIngredients = (TextView) itemView.findViewById(R.id.tv_ingredients);
+            mApplicationContext = applicationContext;
+        }
+
+        public void bindView(List<Ingredient> ingredients){
+            String formattedIngredient;
+            int quantity;
+            String measure;
+            String ingredientDetails;
+
+            for (Ingredient ingredient : ingredients){
+                formattedIngredient  = mApplicationContext.getString(R.string.bullet);
+                quantity = ingredient.getQuantity();
+                measure = ingredient.getMeasure();
+                ingredientDetails = ingredient.getIngredient();
+                formattedIngredient += " " + ingredientDetails + " ("+ quantity +" " + measure + ")";
+                mIngredients.append(formattedIngredient+ "\n\n");
+
+            }
+        }
+    }
+
+
+
+}
